89200c6c36757dc4a0690cfae51f144ab2b19c30
Luigi Leuzzi <leuzzi.luigi98@gmail.com>
2025-09-03 19:17:00 +0200

    project dialog

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,130 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import { Progress } from "@/components/ui/progress"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import Autoplay from 'embla-carousel-autoplay'
+import emblaCarouselVue from 'embla-carousel-vue'
+emblaCarouselVue.globalOptions = { loop: true }
+
+document.documentElement.classList.add('dark')
 
 const projects = [
   {
     image: "src/assets/images/apptor.png",
+    title: "Apptor",
     description: "Low code application - internal Expeed project"
   },
   {
     image: "src/assets/images/schneider-electric.jpg",
+    title: "Schneider Electric",
     description: "Worked on multiple tickets (features/bugs) - Schneider client"
   },
   {
     image: "src/assets/images/unity-tools.jpg",
+    title: "Unity Tools",
     description: "Worked on custom unity tools - Viz Lab"
   },
   {
     image: "src/assets/images/weekly-bucket.jpg",
+    title: "Weekly Bucket",
     description: "Todo App with a twist - College project, front-end"
   },
   {
     image: "src/assets/images/skeleton-rush.png",
+    title: "Skeleton Rush",
     description: "2.5D Run and Shoot game - Personal, Highschool"
   },
   {
     image: "src/assets/images/fearless-run.jpg",
+    title: "Fearless Run",
     description: "2D Dodge game - Personal, college"
   },
 ]
+
+const skills = [
+  {
+    name: 'Angular',
+    value: 90,
+  },
+  {
+    name: '.Net',
+    value: 80,
+  },
+  {
+    name: 'Unity',
+    value: 85,
+  },
+  {
+    name: 'Azure',
+    value: 65,
+  },
+  {
+    name: 'Android Studio',
+    value: 65,
+  },
+  {
+    name: 'HTML',
+    value: 95,
+  },
+  {
+    name: 'CSS',
+    value: 80,
+  },
+  {
+    name: 'Javascript / Typescript',
+    value: 96,
+  },
+  {
+    name: 'Python',
+    value: 80,
+  },
+  {
+    name: 'PHP',
+    value: 70,
+  },
+  {
+    name: 'C#',
+    value: 85,
+  },
+  {
+    name: 'C++',
+    value: 80,
+  },
+  {
+    name: 'Java',
+    value: 70,
+  },
+  {
+    name: 'SQL',
+    value: 90,
+  },
+  {
+    name: 'GraphQL',
+    value: 60,
+  },
+  {
+    name: 'Cypress',
+    value: 70
+  },
+  {
+    name: 'Git / Github',
+    value: 90,
+  },
+  {
+    name: 'Jira',
+    value: 80,
+  },
+];
 
 </script>
 
@@ -53,16 +149,39 @@ const projects = [
     </p>
     <h2>Projects</h2>
     <div class="flex justify-center">
-      <Carousel class="relative w-full max-w-xs md:max-w-lg" :plugins="[Autoplay({
-        delay: 2000,
+      <Carousel loop="true" class="relative w-full max-w-xs md:max-w-lg" :plugins="[Autoplay({
+        delay: 2500,
       })]">
         <CarouselContent>
           <CarouselItem v-for="project in projects" class="md:basis-1/3 lg:basis-1/3">
             <div class="p-1">
               <Card>
-                <CardContent class="flex aspect-video items-center justify-center p-1">
-                  <img :src=project.image></img>
-                </CardContent>
+
+
+                <Dialog>
+                  <DialogTrigger as-child>
+
+
+                    <CardContent class="flex aspect-video items-center justify-center p-1">
+                      <img :src=project.image :alt=project.title></img>
+                    </CardContent>
+
+                  </DialogTrigger>
+                  <DialogContent class="sm:max-w-[425px]">
+                    <DialogHeader>
+                      <DialogTitle>{{ project.title }}</DialogTitle>
+                      <DialogDescription>
+                        {{ project.description }}
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div class="grid gap-4 py-4">
+                      <img :src=project.image :alt=project.title></img>
+
+                    </div>
+                  </DialogContent>
+                </Dialog>
+
+
               </Card>
             </div>
           </CarouselItem>
@@ -73,12 +192,25 @@ const projects = [
     </div>
 
     <h2>Skills</h2>
+    <div v-for="skill in skills">
+      <h4>{{ skill.name }}</h4>
+      <Progress :model-value=skill.value />
+    </div>
+
 
 
   </main>
 </template>
 
 <style scoped>
+h1 {
+  font-size: 2rem;
+}
+
+h2 {
+  font-size: 1.5rem;
+}
+
 h1,
 h2 {
   margin-top: 1rem;
