9759e835b968ba010fe05484c814dbed84833f33
Luigi Leuzzi <leuzzi.luigi98@gmail.com>
2025-09-03 17:57:58 +0200

    projects + shadcn wTailwind

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
+import Autoplay from 'embla-carousel-autoplay'
+
+const projects = [
+  {
+    image: "src/assets/images/apptor.png",
+    description: "Low code application - internal Expeed project"
+  },
+  {
+    image: "src/assets/images/schneider-electric.jpg",
+    description: "Worked on multiple tickets (features/bugs) - Schneider client"
+  },
+  {
+    image: "src/assets/images/unity-tools.jpg",
+    description: "Worked on custom unity tools - Viz Lab"
+  },
+  {
+    image: "src/assets/images/weekly-bucket.jpg",
+    description: "Todo App with a twist - College project, front-end"
+  },
+  {
+    image: "src/assets/images/skeleton-rush.png",
+    description: "2.5D Run and Shoot game - Personal, Highschool"
+  },
+  {
+    image: "src/assets/images/fearless-run.jpg",
+    description: "2D Dodge game - Personal, college"
+  },
+]
+
+</script>
 
 <template>
   <header>
-    <img alt="Avatar" class="avatar" src="./assets/avatar.jpg" width="125" height="125" />
+    <img alt="Avatar" class="avatar" src="./assets/images/avatar.jpg" width="125" height="125" />
     <h1 class="green">Hi, I am Luigi Leuzzi</h1>
     <h3>Full-stack Developer</h3>
     <p> clean and intuitive user interfaces | scalable and performant systems</p>
@@ -19,6 +51,30 @@
       deepened my technical expertise but also fueled my drive to keep growing and contributing to
       impactful software solutions.
     </p>
+    <h2>Projects</h2>
+    <div class="flex justify-center">
+      <Carousel class="relative w-full max-w-xs md:max-w-lg" :plugins="[Autoplay({
+        delay: 2000,
+      })]">
+        <CarouselContent>
+          <CarouselItem v-for="project in projects" class="md:basis-1/3 lg:basis-1/3">
+            <div class="p-1">
+              <Card>
+                <CardContent class="flex aspect-video items-center justify-center p-1">
+                  <img :src=project.image></img>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+
+    <h2>Skills</h2>
+
+
   </main>
 </template>
 
